a9967b17f255f8caf370d9dc4719d18880b53483
Amodhx <amxdhnanditha@gmail.com>
2024-04-26 23:36:31 +0530

    certificate responsive done and added imgs

--- js/index.js
+++ js/index.js
@@ -55,30 +55,27 @@ srtop.reveal('#educationSection>div:nth-child(2)', {delay: 200});
 srtop.reveal('#projectSection>div>div', {delay: 200});
 
 
-
-
-
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 15,
 });
 
 
-let imgs = ["assets/ijse.png","assets/pmv.png","assets/pwc.png"];
-let logo = ["assets/ijsebadge.png","assets/pmvbadge.png","assets/pwcbagde.png"];
-let dates = ["March 2023" , "January 2007 - March 2013","March 2013 - January 2023"];
-let names = ["Institute Of Software Engineering","Pothuwila Maha Vidyalaya","Prince Of Wales College"];
-let para = ["I do my O/L and A/L examinations in this school. I passed my Ol exam in 5 A`s, 2 b`s and 2 C`s. and also i passed my Al exam with # S`s.","",""];
-var  index = 0;
-document.querySelector("#educationSection>div:first-child>input").addEventListener('click',async () => {
-    if (index == 0){
+let imgs = ["assets/ijse.png", "assets/pmv.png", "assets/pwc.png"];
+let logo = ["assets/ijsebadge.png", "assets/pmvbadge.png", "assets/pwcbagde.png"];
+let dates = ["March 2023", "January 2007 - March 2013", "March 2013 - January 2023"];
+let names = ["Institute Of Software Engineering", "Pothuwila Maha Vidyalaya", "Prince Of Wales College"];
+let para = ["I do my O/L and A/L examinations in this school. I passed my Ol exam in 5 A`s, 2 b`s and 2 C`s. and also i passed my Al exam with # S`s.", "", ""];
+var index = 0;
+document.querySelector("#educationSection>div:first-child>input").addEventListener('click', async () => {
+    if (index == 0) {
         $('#educationSection>div:nth-child(2)>img:first-child').css({
-            top:"calc(25vh + 5px)"
+            top: "calc(25vh + 5px)"
         })
         $('#educationSection>div:nth-child(2)>img:nth-child(2)').css({
             top: "6vh"
         })
 
-    }else {
+    } else {
         $('#educationSection>div:nth-child(2)>img:first-child').css({
             top: "calc(19vh + 4px)"
         })
@@ -118,28 +115,27 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-$("#educationSection>div:first-child").on('mouseenter',()=>{
+$("#educationSection>div:first-child").on('mouseenter', () => {
     $("#educationSection>div:first-child>input").addClass("btn");
 })
-$("#educationSection>div:first-child").on('mouseleave',()=>{
+$("#educationSection>div:first-child").on('mouseleave', () => {
     $("#educationSection>div:first-child>input").removeClass("btn");
 })
 
 // resume
-$("#resumeSection>div>div").on('mouseenter',()=>{
-   $("#resumeSection>div>div>div:first-child").addClass("showIcon") ;
-   $("#resumeSection>div>div>div:nth-child(2)").addClass("hidetext") ;
+$("#resumeSection>div>div").on('mouseenter', () => {
+    $("#resumeSection>div>div>div:first-child").addClass("showIcon");
+    $("#resumeSection>div>div>div:nth-child(2)").addClass("hidetext");
 });
-$("#resumeSection>div>div").on('mouseleave',()=>{
-    $("#resumeSection>div>div>div:first-child").removeClass("showIcon") ;
-    $("#resumeSection>div>div>div:nth-child(2)").removeClass("hidetext") ;
+$("#resumeSection>div>div").on('mouseleave', () => {
+    $("#resumeSection>div>div>div:first-child").removeClass("showIcon");
+    $("#resumeSection>div>div>div:nth-child(2)").removeClass("hidetext");
 });
-
 
 
 // projects
-$("#projectSection>div>div:first-child").on('mouseenter',()=>{
-    $("#projectSection>div>div:first-child>div").addClass("fadeCir") ;
+$("#projectSection>div>div:first-child").on('mouseenter', () => {
+    $("#projectSection>div>div:first-child>div").addClass("fadeCir");
 
     $("#projectSection>div>div:first-child>h4").addClass("textClr");
     $("#projectSection>div>div:first-child>h1").addClass("textClr");
@@ -148,8 +144,8 @@ $("#projectSection>div>div:first-child").on('mouseenter',()=>{
     $("#projectSection>div>div:first-child>img").addClass("imgClr");
 });
 
-$("#projectSection>div>div:nth-child(2)").on('mouseenter',()=>{
-    $("#projectSection>div>div:nth-child(2)>div").addClass("fadeCir") ;
+$("#projectSection>div>div:nth-child(2)").on('mouseenter', () => {
+    $("#projectSection>div>div:nth-child(2)>div").addClass("fadeCir");
 
     $("#projectSection>div>div:nth-child(2)>h4").addClass("textClr");
     $("#projectSection>div>div:nth-child(2)>h1").addClass("textClr");
@@ -157,8 +153,8 @@ $("#projectSection>div>div:nth-child(2)").on('mouseenter',()=>{
 
     $("#projectSection>div>div:nth-child(2)>img").addClass("imgClr");
 });
-$("#projectSection>div>div:nth-child(3)").on('mouseenter',()=>{
-    $("#projectSection>div>div:nth-child(3)>div").addClass("fadeCir") ;
+$("#projectSection>div>div:nth-child(3)").on('mouseenter', () => {
+    $("#projectSection>div>div:nth-child(3)>div").addClass("fadeCir");
 
     $("#projectSection>div>div:nth-child(3)>h4").addClass("textClr");
     $("#projectSection>div>div:nth-child(3)>h1").addClass("textClr");
@@ -167,8 +163,8 @@ $("#projectSection>div>div:nth-child(3)").on('mouseenter',()=>{
     $("#projectSection>div>div:nth-child(3)>img").addClass("imgClr");
 });
 
-$("#projectSection>div>div").on('mouseleave',()=>{
-    $("#projectSection>div>div>div").removeClass("fadeCir") ;
+$("#projectSection>div>div").on('mouseleave', () => {
+    $("#projectSection>div>div>div").removeClass("fadeCir");
 
     $("#projectSection>div>div>h4").removeClass("textClr");
     $("#projectSection>div>div>h1").removeClass("textClr");
@@ -178,26 +174,71 @@ $("#projectSection>div>div").on('mouseleave',()=>{
 });
 
 
-$("#projectSection>div>div:first-child>nav").on('mouseenter',()=>{
+$("#projectSection>div>div:first-child>nav").on('mouseenter', () => {
     $("#projectSection>div>div:first-child>nav>div:first-child").addClass("showicon");
     $(" #projectSection>div>div:first-child>nav>div:nth-child(2)").addClass("hideText");
     $(" #projectSection>div>div:first-child>nav").addClass("clr")
 })
-$("#projectSection>div>div:nth-child(2)>nav").on('mouseenter',()=>{
+$("#projectSection>div>div:nth-child(2)>nav").on('mouseenter', () => {
     $("#projectSection>div>div:nth-child(2)>nav>div:first-child").addClass("showicon");
     $(" #projectSection>div>div:nth-child(2)>nav>div:nth-child(2)").addClass("hideText");
     $(" #projectSection>div>div:nth-child(2)>nav").addClass("clr")
 })
 
-$("#projectSection>div>div:nth-child(3)>nav").on('mouseenter',()=>{
+$("#projectSection>div>div:nth-child(3)>nav").on('mouseenter', () => {
     $("#projectSection>div>div:nth-child(3)>nav>div:first-child").addClass("showicon");
     $(" #projectSection>div>div:nth-child(3)>nav>div:nth-child(2)").addClass("hideText");
     $(" #projectSection>div>div:nth-child(3)>nav").addClass("clr")
 })
-$("#projectSection>div>div>nav").on('mouseleave',()=>{
+$("#projectSection>div>div>nav").on('mouseleave', () => {
     $("#projectSection>div>div>nav>div:first-child").removeClass("showicon");
     $(" #projectSection>div>div>nav>div:nth-child(2)").removeClass("hideText");
     $(" #projectSection>div>div>nav").removeClass("clr")
 })
 
 
+// certificate description viewer
+
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(1)>img").on('click', () => {
+    textAppend(0);
+
+});
+
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(2)>img").on('click', () => {
+    textAppend(1);
+
+});
+
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(3)>img").on('click', () => {
+    textAppend(2);
+
+});
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(4)>img").on('click', () => {
+    textAppend(3);
+
+});
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(5)>img").on('click', () => {
+    textAppend(4);
+
+});
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(6)>img").on('click', () => {
+    textAppend(5);
+
+});
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(7)>img").on('click', () => {
+    textAppend(6);
+
+});
+$("#certificateSection>div:nth-child(2)>div:nth-child(2)>div:nth-child(8)>img").on('click', () => {
+    textAppend(7);
+});
+
+$("#certificateSection>div:nth-child(2)>div:first-child>nav>img").on('click', () => {
+    $("#certificateSection>div:nth-child(2)>div:first-child").removeClass("showSQ");
+})
+
+let ar = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
+
+function textAppend(index) {
+    $("#certificateSection>div:nth-child(2)>div:first-child").addClass("showSQ");
+}
