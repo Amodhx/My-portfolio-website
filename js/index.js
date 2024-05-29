280dc12f9e5c6d9be39aa5d695de70682f6b3099
Amodhx <amxdhnanditha@gmail.com>
2024-05-29 19:12:05 +0530

    all assignment added

--- js/index.js
+++ js/index.js
@@ -346,34 +346,34 @@ $("#assignmentSection > nav > div:nth-child(2)>div:nth-child(2)>img:nth-child(2)
     window.open('https://amodhx.github.io/My-portfolio-website/');
 });
 $("#assignmentSection > nav > div:nth-child(3)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open('https://amodhx.github.io/Assignment03-WEB/');
 });
 $("#assignmentSection > nav > div:nth-child(4)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/IT-assignment04/");
 });
 $("#assignmentSection > nav > div:nth-child(5)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
     window.open('https://amodhx.github.io/My-portfolio-website/');
 });
 $("#assignmentSection > nav > div:nth-child(6)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/Inventory-management-system/");
 });
 $("#assignmentSection > nav > div:nth-child(7)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/Inventory-management-system/");
 });
 $("#assignmentSection > nav > div:nth-child(8)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/Assignment-08/");
 });
 $("#assignmentSection > nav > div:nth-child(9)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/IT-assignment09/");
 });
 $("#assignmentSection > nav > div:nth-child(10)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/Assignment-10/");
 });
 $("#assignmentSection > nav > div:nth-child(11)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/Inventory-management-system/");
 });
 $("#assignmentSection > nav > div:nth-child(12)>div:nth-child(2)>img:nth-child(2)").on('click', () => {
-
+    window.open("https://amodhx.github.io/Inventory-management-system/");
 });
 //assignment view in path end
 
@@ -413,7 +413,7 @@ $("#assignmentSection > nav > div:nth-child(11)>div:nth-child(2)>img:first-child
     window.open('https://github.com/Amodhx/Inventory-management-system');
 });
 $("#assignmentSection > nav > div:nth-child(12)>div:nth-child(2)>img:first-child").on('click', () => {
-
+    window.open('https://github.com/Amodhx/Inventory-management-system');
 });
 
 // assignment github linkadding end
